094d4615dd957ee9ee9cfbdbae600e3fa1a81acc
vickychandra357 <81799094+vickychandra357@users.noreply.github.com>
2023-01-27 15:23:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 //your code here
-function countWords(obj){
-    document.getElementById("wordCount").innerHTML = obj.value.length;
+function wordCount() {
+ var text = document.getElementById("evaluatedText").value;
+ var count = 0;
+ var split = text.split(' ');
+ for (var i = 0; i < split.length; i++) {
+  if (split[i] != "") {
+   count ++;
+  }
+ }
+ document.getElementById("wordCount").innerHTML = count;
 }
